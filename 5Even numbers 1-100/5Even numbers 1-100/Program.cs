e51103a0a5ee8f1d02f1ab08b1762eeb58a52847
AhmadSaeed6802 <139094382+AhmadSaeed6802@users.noreply.github.com>
2023-07-14 04:02:58 -0700

    Add files via upload

---
+++ 5Even numbers 1-100/5Even numbers 1-100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5Even_numbers_1_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers from 1-100");// print a message to inform the user about what is going to be done
+            for (int i = 1; i <= 100; i++)          // for loop to iterate from 1 to 100
+            {
+                if (i % 2 == 0)             // cheking condition for even number
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
